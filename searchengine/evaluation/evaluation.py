c3e0004ef14abda0f8e3386041473008ef008b1e
Alizee <hellas90@gmail.com>
2015-01-05 23:16:05 +0100

    plot eval & rappel

--- searchengine/evaluation/evaluation.py
+++ searchengine/evaluation/evaluation.py
@@ -1,5 +1,6 @@
 import string
 from searchengine.search.vectorial_search import vectorial_search
+import matplotlib.pyplot as plt
 
 class Request:
 
@@ -30,3 +31,23 @@ def rappel(request, document_list, common_words, rank):
     search_result = vectorial_search(request.text, document_list, common_words, rank)
     return number_of_relevant_documents(request, search_result)/len(request.result)
 
+def plot_precision_rappel(request, document_list, common_words):
+    fig, ax1 = plt.subplots()
+    plt.axis([0, len(document_list), 0, 100])
+    plt.xlabel('Rank')
+    ax1.set_ylabel('Precision', color='r')
+    plt.title('Evaluation')
+    yaxis = []
+    for rank in range(1,len(document_list)):
+        yaxis.append(precision(request, document_list, common_words, rank)*100)
+    ax1.plot(range(1,len(document_list)), yaxis, 'ro')
+        
+    ax2 = ax1.twinx()
+    plt.axis([0, len(document_list), 0, 100])
+    ax2.set_ylabel('Rappel', color='b')
+    plt.title('Evaluation')
+    yaxis2 = []
+    for rank in range(1,len(document_list)):
+        yaxis2.append(rappel(request, document_list, common_words, rank)*100)
+    ax2.plot(range(1,len(document_list)), yaxis2, 'bo')
+    plt.show()
