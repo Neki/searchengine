c3f9a003e2ec333cf1669f344c709688c63f9445
Benoît Faucon <faucon.benoit@cegetel.net>
2015-01-05 19:46:10 +0100

    Correctly parse the user request (for boolean search).

--- searchengine/search/boolean_search.py
+++ searchengine/search/boolean_search.py
@@ -1,6 +1,7 @@
-from searchengine.index.process import InvertedIndex
-from searchengine.parser import RequestDocument
-    
+import string
+import re
+
+
 class Node:
     def __init__(self, *children):
         self._children = children
@@ -8,114 +9,191 @@ class Node:
     @property
     def children(self):
         return self.__children
-    
-class AndNode:
-    def __init__(self,left,right):
-        super().__init__(left,right)
-        
+
+
+class AndNode(Node):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+
     def eval(self):
         for child in self._children:
             if not child.eval():
                 return False
         return True
-    
-class OrNode:
-    def __init__(self,left,right):
-        super().__init__(left,right)
-    
+
+    def __repr__(self):
+        return "AndNode( ({0}) and ({1}) )".format(self._children[0], self._children[1])
+
+
+class OrNode(Node):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+
     def eval(self):
         for child in self._children:
             if child.eval():
                 return True
         return False
-    
-class NotNode:
-     def __init__(self, right):
-        super().__init__(right)    
-        
-     def eval(self):
-        assert(size(self._children)==1)
+
+    def __repr__(self):
+        return "OrNode( ({0}) or ({1}) )".format(self._children[0], self._children[1])
+
+
+class NotNode(Node):
+    def __init__(self, right):
+        super().__init__(right)
+
+    def eval(self):
+        assert(len(self._children) == 1)
         return not self._children[0].eval()
-        
-class WordNode:
-    def __init__(self,index, word):
+
+    def __repr__(self):
+        return "NotNode( not {0} )".format(self._children[0])
+
+
+class WordNode(Node):
+    def __init__(self, index, word):
         self.index = index
         self.word = word
-        
+
     def eval(self, doc_id):
         return self.word in self.index.get_words_by_doc_id(doc_id)
-            
-def is_well_parenthesized(request):
-    pile = []
-    for c in request:
-        if c in "([{":
-            pile.append(c)
-        elif c in ")]}":
-            if length(pile) == 0:
-                return False
-            else:
-                temp = top(pile)
-                pile = pop(pile)
-                if c!=temp:
-                    return False
-    if length(pile) == 0:
-        return true
-    else:
-        return False
+
+    def __repr__(self):
+        return "WordNode( {0} )".format(self.word)
+
 
 def tokenize_request(request):
-    cleaned = ''.join([_punctuation_to_space(c) for c in list(line)])
-    tokens = [token for token in cleaned.split(" ") if token not in string.whitespace]
-    return tokens
+    """
+    Example:
+       >>> tokenize_request("a and b,c or (d and e)")
+       ["a", "and", "b", "c", "or", "(", "d", "and", "e", ")"]
+    """
+    return [t for t in re.split('(\W)', request) if t == "(" or t == ")" or not (t in string.whitespace or t in string.punctuation)]
 
-def _punctuation_to_space(character):
-    separators = string.punctuation
-    for c in "{[(])}":
-        separators.replace(c,"")
-    if character in separators:
-        return " "
-    return character
-    
-def build_tree(request):
-    operators=["and","or", "not"]
-    t = WordNode({},"")
-    p = []
-    r = tokenize_request(request)
-    for c in r:
-        if c in "([{":
-            p.append(t)
-            t.children[0] = WordNode({},"")
-            t = t.children[0]
-        elif c in operators:
-            if c in ["and", "or"]:
-                rac = top(pi)
-                p = pop(p)
-                rac.rac = c
-                rac.children[0] = t
-                p.append(rac)
-                rac.children[1] = WordNode({},"")
-                t = rac.children[1]
+
+def build_tree(request, index):
+    """
+    Build a request boolean tree from a request.
+    Parameters:
+        request (str): a request ("a and (b or c)")
+        index (InvertedIndex)
+    Returns:
+        (Node) the root node of the request tree
+    """
+    tokens = tokenize_request(request)
+    tokens = add_missing_ands(tokens)
+    return tokens_to_node(tokens, index)
+
+
+def add_missing_ands(tokens):
+    """
+    Parameter:
+        tokens (list of str)
+    Returns:
+        (list of str)
+    Example:
+        >>> add_missing_ands(["a", "b", "(","a", "b",")", "e"])
+        ["a", "and", "b", "and", "(", "a", "and", "b", ")", "and", "e"]
+    """
+    out = []
+    for i in range(0, len(tokens) - 1):
+        a = tokens[i]
+        b = tokens[i+1]
+        out.append(a)
+        if (is_word(a) and is_word(b)) or (a == ")" and is_word(b)) or (is_word(a) and b == "("):
+            out.append("and")
+    out.append(tokens[-1])
+    return out
+
+
+def is_word(token):
+    return token not in ["and", "or", "not", ")", "("]
+
+
+class MismatchedParens(Exception):
+    pass
+
+
+def tokens_to_node(tokens, index):
+    """
+    Transforms a list of tokens into a boolean tree. Respects operator precedence and parenthesis.
+    Parameters:
+        tokens (list of str)
+        index (InvertedIndex)
+    Returns:
+        Node
+    """
+    # the implementation is an adaptation of the shunting-yard algorithm
+    stack = []  # a stack of (pending) operators
+    output = []  # a stack containing nodes
+                 # at the end of the algorithm, this stack will contain only the root of the tree
+    operators = {"and": 2, "or": 1, "not": 3}  # boolean operators with their precedence
+    for token in tokens:
+        #print("token: {0} / output: {1} / stack: {2}".format(token, output, stack))
+        if token in operators:
+            if len(stack) == 0:
+                stack.append(token)
             else:
-                rac = top(pi)
-                p = pop(p)
-                rac.rac = c
-                rac.children[0] = WordNode({},"")
-                p.append(rac)
-                rac.children[1] = WordNode({},"")
-                t = rac.children[1]
-        elif c in ")]}":
-            rac = top(pi)
-            p = pop(p)
-            rac.children[1] = t
-            t = rac
-        else:
-            t = WordNode({},"")
-    if length(p) == 0:
-        return t
-    else:
-        raise "Not well parenthesized"
-            
+                top = stack[-1]
+                while(top in operators and operators[top] > operators[token]):
+                    top = stack.pop()
+                    _build_node(top, stack, output)
+                    if len(stack) == 0:
+                        break
+                    top = stack[-1]
+                stack.append(token)
+        elif token == "(":
+            stack.append(token)
+            output.append("(")
+        elif token == ")":
+            if len(stack) == 0:
+                raise MismatchedParens
+            top = stack.pop()
+            while top != "(":
+                if len(stack) == 0:
+                    raise MismatchedParens
+                _build_node(top, stack, output)
+                top = stack.pop()
+            assert(len(output) >= 2)
+            top = output.pop()
+            paren = output.pop()
+            if paren != "(":
+                raise MismatchedParens
+            output.append(top)
+        else:  # the token is a word
+            output.append(WordNode(index, token))
+    #print("no more tokens")
+    while(len(stack) > 0):
+        #print("output: {0} / stack: {1}".format(output, stack))
+        top = stack.pop()
+        if top == "(" or top == ")":
+            raise MismatchedParens
+        _build_node(top, stack, output)
+    assert(len(output) == 1)
+    return output[0]
+
+
+# Used inside the above method
+def _build_node(node_type, stack, output):
+    assert node_type in ["and", "or", "not"]
+    if node_type == "not":
+        assert len(output) >= 1
+        output.append(NotNode(output.pop()))
+        return
+    assert len(output) >= 2
+    left = output.pop()
+    if left == "(":
+        raise MismatchedParens
+    right = output.pop()
+    if right == "(":
+        raise MismatchedParens
+    if node_type == "and":
+        output.append(AndNode(left, right))
+    elif node_type == "or":
+        output.append(OrNode(left, right))
+    #print("applied operator: output : {0} / stack : {1}".format(output, stack))
+
+
 def boolean_search(request, document_list, common_words):
-    #for doc in document_list: 
-    #    return tree.eval(doc)
     pass
--- searchengine/tests/search/test_boolean_search.py
+++ searchengine/tests/search/test_boolean_search.py
@@ -1,15 +1,37 @@
 from unittest import TestCase
-from searchengine.search.boolean_search import *
+from searchengine.search.boolean_search import WordNode, build_tree, tokenize_request, MismatchedParens, add_missing_ands
+from searchengine.index.process import InvertedIndex
 from searchengine.parser.cacm_document import CacmDocument
 
+
 class TestBooleanSearch(TestCase):
-    
+
     def test_eval(self):
         document = CacmDocument(2, "common title", "not so common abstract", "unusual keywords")
         index = InvertedIndex(["common", "not", "so"], [document])
-        word_node = WordNode(index,"goal")
-        self.assertGreaterEqual(False,word_node.eval(2))
+        word_node = WordNode(index, "goal")
+        self.assertGreaterEqual(False, word_node.eval(2))
 
     def test_build_tree(self):
-        request="( ( toto and titty ) or ( goal or food ) ) or (not tata)"
-        build_tree(request)
+        # Test that requests are parsed without errors
+        index = None
+        requests = ["a b or c", "a and (b or c)", "c and ((b or c) or (a and (c and d)))", "a and (b c)"]
+        for request in requests:
+            build_tree(request, index)
+        # Test that badly formed requests are rejected
+        requests = ["a (and b or c", "a (and b) or c", "a or b and c )"]
+        for request in requests:
+            self.assertRaises(MismatchedParens, build_tree, request, index)
+
+    def test_tokenize_request(self):
+        request = "toto and (tata or not titi)"
+        self.assertEqual(["toto", "and", "(", "tata", "or", "not",  "titi", ")"], tokenize_request(request))
+
+    def test_add_missing_ands(self):
+        base = ["toto", "or", "(", "tata", "titi", ")", "tutu"]
+        expected = ["toto", "or", "(", "tata", "and", "titi", ")", "and", "tutu"]
+        self.assertEqual(expected, add_missing_ands(base))
+
+
+
+
