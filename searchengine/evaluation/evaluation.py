35b90ccc703ab5f9a509ac2ecaed4b0e030365c3
Alizee <hellas90@gmail.com>
2015-01-10 17:34:12 +0100

    courbe rappel précision

--- searchengine/evaluation/evaluation.py
+++ searchengine/evaluation/evaluation.py
@@ -21,6 +21,8 @@ def precision(request, search_results):
     """ 
     Number of relevant documents found over number of found documents
     """
+    print("relevant docs found = "+str(number_of_relevant_documents(request, search_results)))
+    print("docs found = " + str(len(search_results)))
     return number_of_relevant_documents(request, search_results)/len(search_results)
     
 def rappel(request, search_results):
@@ -28,6 +30,21 @@ def rappel(request, search_results):
     Number of relevant documents found over number of relevant documents
     """
     return number_of_relevant_documents(request, search_results)/len(request.result)
+
+def courbe_rappel_precision(request, search_results):
+    fig, ax1 = plt.subplots()
+    plt.axis([0, 100, 0, 100])
+    plt.xlabel('Rappel')
+    plt.ylabel('Precision')
+    plt.title('Evaluation')
+    xaxis = []
+    yaxis = []
+    for rank in range(1,len(search_results)):
+        xaxis.append(precision(request, search_results[:rank])*100)
+        yaxis.append(rappel(request, search_results[:rank])*100)
+        print(" rappel x = " + str(xaxis[len(xaxis)-1])+"\n")
+    plt.plot(xaxis, yaxis)
+    plt.show()
 
 def plot_precision_rappel(request, search_results):
     fig, ax1 = plt.subplots()
