eb352dc428dd82f11bc7f4b94688419ca69f2ba6
Benoît Faucon <faucon.benoit@cegetel.net>
2015-01-12 21:35:20 +0100

    Simplify two unit tests so they are really "unit" tests.

--- searchengine/tests/evaluation/test_evaluation.py
+++ searchengine/tests/evaluation/test_evaluation.py
@@ -8,22 +8,14 @@ from searchengine.index.process import Weighting
 class TestEvaluation(TestCase):
 
     def test_number_of_relevant_documents(self):
-        request = Request(1, "tata", [1,2])
-        document1 = CacmDocument(1, "aujourd \n toto \n tata","il fait \n tyty","toto \n tata tyty")
-        document2 = CacmDocument(2,"plouf \n paf","tata \n toto il tata","tyty plouf \n tata paf")
-        document_list = [document1, document2]
-        common_words=["aujourd","il","fait","ca","mot","ok"]
-        search_result = vectorial_search(request.text, document_list, common_words, 2,Weighting.TermFrequency)
-        self.assertEqual(2,number_of_relevant_documents(request, search_result))
+        request = Request(1, "tata", [1, 2, 5, 12])
+        search_result = [(1, 20), (2, 3), (4, 5), (7, 2)]
+        self.assertEqual(2, number_of_relevant_documents(request, search_result))
 
     def test_precision(self):
-        request = Request(1,"tata",[1,2])
-        document1 = CacmDocument(1, "aujourd \n toto \n tata","il fait \n tyty","toto \n tata tyty")
-        document2 = CacmDocument(2,"plouf \n paf","tata \n toto il tata","tyty plouf \n tata paf")
-        document_list = [document1, document2]
-        common_words=["aujourd","il","fait","ca","mot","ok"]
-        search_results = vectorial_search(request.text, document_list, common_words, 2,Weighting.TermFrequency)
-        self.assertEqual(1,precision(request, search_results))
+        request = Request(1, "tata", [1, 2, 5, 12])
+        search_result = [(1, 20), (2, 3), (4, 5), (7, 2)]
+        self.assertEqual(0.5, precision(request, search_result))
 
     def test_rappel(self):
         request = Request(1,"tata",[1,2])
