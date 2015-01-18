e4992d94a9f63d87cba02c91cb1deb90faecb833
Benoît Faucon <faucon.benoit@cegetel.net>
2015-01-18 16:32:09 +0100

    Better search test.

--- searchengine/tests/search/test_probability_search.py
+++ searchengine/tests/search/test_probability_search.py
@@ -8,10 +8,10 @@ class TestProbabilitySearch(TestCase):
 
     def test_binary_independance_retrieval(self):
         request = "tata"
-        document1 = CacmDocument(1, "aujourd \n toto \n tata","il fait \n tyty","toto \n tata tyty")
-        document2 = CacmDocument(2,"plouf \n paf","tata \n toto il tata","tyty plouf \n tata paf")
+        document1 = CacmDocument(1, "aujourd \n toto \n" ,"il fait \n tyty", "toto \n tyty")
+        document2 = CacmDocument(2, "plouf \n paf", "tata \n toto il tata", "tyty plouf \n tata paf")
         document_list = [document1, document2]
-        common_words=["aujourd","il","fait","ca","mot","ok"]
+        common_words = ["aujourd", "il", "fait", "ca", "mot", "ok"]
         search_result = binary_independance_retrieval(request, document_list, common_words, 2)
-        self.assertCountEqual([(document2.doc_id,2),(document1.doc_id,3)], search_result)
+        self.assertEqual(2, search_result[0][0])
 
