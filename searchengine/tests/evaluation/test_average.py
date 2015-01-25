b4f98534ed95290318a36cdaa2b5ddf9e1d2f9a6
Benoît Faucon <faucon.benoit@cegetel.net>
2015-01-25 17:54:34 +0100

    Fix test_compute_average_precision.

--- searchengine/tests/evaluation/test_average.py
+++ searchengine/tests/evaluation/test_average.py
@@ -8,6 +8,6 @@ class TestAverage(TestCase):
     def test_compute_average_precision(self):
         requests = [Request(1, "tata", [1, 2, 3]), Request(2, "toto", [3, 4])]
         search_results = {1: [(2, 2), (3, 1)], 2: [(4, 1)]}
-        avg_precision = compute_avg_precision(requests, search_results, [0, 0.5, 1])
+        avg_precision = compute_avg_precision(requests, search_results, [0, 0.5, 1], 3)
         self.assertEqual([0, 0.5, 1], [p[0] for p in avg_precision])
-        self.assertEquals((0, 1), avg_precision[0])        
+        self.assertEquals((0, 1), avg_precision[0])
